c3d4773c26e611fcbb460c74ad11e03ec1e34b9e
Ted Roden <tedroden@gmail.com>
2014-10-22 17:10:01 -0400

    An update. I deleted init.el and tried to redo it.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Ted Roden's .emacs.d directory
+
+I've totally redone my .emacs.d stuff. 
+
+This sets up some sane (IMO) defaults and installs useful packages.
+
+It allows for two special files:
+
+1. `custom.el`: will store things set with `custom`
+2. `personal.el`: We'll load this if it exists for things that don't belong in git. 
+
+Let me know what you think
---
+++ init.el
@@ -0,0 +1,89 @@
+;; rethinking everything
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1)) 
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tooltip-mode) (tooltip-mode -1))
+
+(setq dotfiles-dir "~/.emacs.d/")
+(setq lisp-dir (concat dotfiles-dir "lisp"))
+(add-to-list 'load-path lisp-dir)
+
+(setq custom-file (concat dotfiles-dir "custom.el"))
+(setq personal-file (concat dotfiles-dir "personal.el"))
+(dolist (f (list custom-file personal-file))
+  (if (file-exists-p f)
+	  (progn (load f)
+		 (message (concat "Loaded " f)))
+	nil))
+
+;; good mode.
+(ido-mode)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+;; a list of packages
+(defvar tedroden/packages
+  '(auto-complete
+    color-theme
+    color-theme-solarized
+    ac-js2
+    ac-ispell
+    go-autocomplete
+    magit
+    markdown-mode
+    )
+  "Stuff I like")
+
+(dolist (p tedroden/packages)
+  (when (not (package-installed-p p))
+	(package-install p)))
+
+;; auto complete setup. Is this right?
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories (concat lisp-dir "ac-dict"))
+(ac-config-default)
+
+;; magit setup. Is this right?
+(setq magit-dir (concat dotfiles-dir "external/magit/"))
+(add-to-list 'load-path magit-dir)
+(require 'magit)
+(global-set-key (kbd "C-c m") 'magit-status)
+
+
+(global-hi-lock-mode 1)	  
+(defun tedroden/code-setup ()
+  (interactive)
+  (highlight-phrase "FIXME" 'hi-yellow)
+  (highlight-phrase "Fixme" 'hi-yellow)
+  (highlight-phrase "fixme" 'hi-yellow)
+  (highlight-phrase "TODO" 'hi-yellow)
+  (highlight-phrase "Todo" 'hi-yellow)
+  (highlight-phrase "todo" 'hi-yellow))
+
+(add-hook 'prog-mode-hook  'tedroden/code-setup)
+
+;; setup-x p goes to the previous window (opposite of C-x o)
+(defun tedroden/prev-window ()
+"go to previous window"
+(interactive)
+(other-window -1))
+
+;; what do you think this does?
+(setq-default tab-width 4)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(defun tedroden/edit-dot-emacs ()
+  (interactive)
+  (find-file (concat dotfiles-dir "init.el")))
+
+;; set up some key bindings (setq default-tab-width 4)
+(global-set-key "\M-g" 'goto-line)	
+(global-set-key "\M-_" 'shrink-window)
+(global-set-key "\M-+" 'enlarge-window)
+(global-set-key (kbd "C-x p") 'tedroden/prev-window)
+(global-set-key [f4] 'tedroden/edit-dot-emacs)
