81c7073fd8ddc52850c62a2ed72b93f67974733f
Ted Roden <tedroden@gmail.com>
2016-03-17 11:50:07 -0400

    it's a bit customized for me, but some improvements
    i'll have to generalize this

--- dot-emacs.el
+++ dot-emacs.el
@@ -8,6 +8,10 @@
 (setq lisp-dir (concat dotfiles-dir "lisp"))
 (add-to-list 'load-path lisp-dir)
 
+;; FIXME: remove paths to /home/troden
+;; FIXME: add artbollocks-mode,
+;; FIXME: flyspell to markdown mode
+
 (setq custom-file (concat dotfiles-dir "custom.el"))
 (setq personal-file (concat dotfiles-dir "personal.el"))
 (dolist (f (list custom-file personal-file))
@@ -22,7 +26,8 @@
 ;; confirm on exit (i have fat fingers on one of my keyboards)
 (setq confirm-kill-emacs 'yes-or-no-p)
 
-; (set-default-font "Hack 14")
+;; FIXME: if we're on HiDPI 
+(set-default-font "Hack 14")
 
 (require 'package)
 ;; (add-to-list 'package-archives
@@ -45,14 +50,12 @@
 	rainbow-blocks
 	rainbow-delimiters
 	rainbow-mode
-	swiper
-	counsel
 	use-package
 	smooth-scroll
 	js3-mode
 	beacon
 	paradox
-	base16-theme	
+	base16-theme
     )
   "Stuff I like")
 
@@ -67,33 +70,39 @@
 
 
 (global-set-key (kbd "M-o") 'ace-window)
-(global-set-key (kbd "C-'") 'avy-goto-char-2)
-
-(ivy-mode t) ; \#\|\.\#\|\~\|\.pyc$\|\.DS_Store\|__pycache (filefind ignore regexp)
+(global-set-key (kbd "C-.") 'avy-goto-char-2)
 
 
-;;; highlight the cursor when scrolling or switching buffers
-;; super great for teaching or for someone looking over your shoulder
-(beacon-mode 1) 
+(use-package beacon-mode
+  :init
+  (beacon-mode 1))
 
-(setq ivy-display-style 'fancy)
-(setq ivy-count-format "(%d/%d) ")
+(use-package markdown-mode
+  :init
+  (setq markdown-list-indent-width 2))
 
-(require 'counsel)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "C-h f") 'counsel-describe-function)
-(global-set-key (kbd "C-h v") 'counsel-describe-variable)
-(global-set-key (kbd "C-h l") 'counsel-load-library)
-; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-;(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-c l") 'counsel-locate)
-; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+;; ;;; highlight the cursor when scrolling or switching buffers
+;; ;; super great for teaching or for someone looking over your shoulder
+;; (beacon-mode 1) 
 
-(setq ivy-use-virtual-buffers t)
+;; ivy was cool. using helm now though
+;; (ivy-mode nil) 
+;; (setq ivy-display-style 'fancy)
+;; (setq ivy-count-format "(%d/%d) ")
+;; (require 'counsel)
+;; (global-set-key (kbd "M-x") 'counsel-M-x)
+;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+;; (global-set-key (kbd "C-h f") 'counsel-describe-function)
+;; (global-set-key (kbd "C-h v") 'counsel-describe-variable)
+;; (global-set-key (kbd "C-h l") 'counsel-load-library)
+;; ; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+;; ;(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+;; (global-set-key (kbd "C-c g") 'counsel-git)
+;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
+;; (global-set-key (kbd "C-c k") 'counsel-ag)
+;; (global-set-key (kbd "C-c l") 'counsel-locate)
+;; ; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+;; (setq ivy-use-virtual-buffers t)
 
 (require 'use-package)
 (use-package smooth-scroll
@@ -110,17 +119,51 @@
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode))
 
+(use-package undo-tree
+  :defer t
+  :ensure t
+  :diminish undo-tree-mode
+  :config
+  (progn
+    (global-undo-tree-mode)
+    (setq undo-tree-visualizer-timestamps t)
+    (setq undo-tree-visualizer-diff t)))
+
+(use-package artbollocks-mode
+  :defer t
+  :config
+  (progn
+    (setq artbollocks-weasel-words-regex
+          (concat "\\b" (regexp-opt
+                         '("one of the"
+                           "should"
+                           "just"
+                           "sort of"
+                           "a lot"
+                           "probably"
+                           "maybe"
+                           "perhaps"
+                           "I think"
+                           "really"
+                           "pretty"
+                           "nice"
+                           "action"
+                           "utilize"
+                           "leverage") t) "\\b"))
+    ;; Don't show the art critic words, or at least until I figure
+    ;; out my own jargon
+    (setq artbollocks-jargon nil)))
 
 ;; ido is a good mode.
 (ido-mode t)
 (ido-everywhere t)
 (flx-ido-mode t)
 
-; (load-theme 'twilight-bright)
-; (load-theme 'twilight-anti-bright)
 (load-theme 'base16-twilight-dark)
-(global-linum-mode t)
-; (global-hl-line-mode nil)
+
+;; (global-hl-line-mode nil)
+; (global-linum-mode t) ;; this is good for teaching, but i don't generally want it.
+(column-number-mode t)
 
 ;; auto complete setup. Is this right?
 (require 'auto-complete-config)
@@ -148,6 +191,7 @@
 (add-hook 'text-mode-hook  'tedroden/code-setup)
 
 (add-hook 'css-mode-hook 'rainbow-mode)
+(add-hook 'sass-mode-hook 'rainbow-mode)
 
 ;; setup-x p goes to the previous window (opposite of C-x o)
 (defun tedroden/prev-window ()
@@ -203,7 +247,61 @@
 (add-hook 'eshell-mode-hook 'eshell-mode-hook-func)
 
 ;; start the server
-(server-start)
+;; FIXME: can we check to see if it's running first?
+;; (server-start)
+
+
 ;; this *should* make it so we don't open new frames
 (setq ns-pop-up-frames nil)
+
+;; (setq c-default-style "linux")
+(setq-default c-basic-offset 8
+	      tab-width 8
+	      indent-tabs-mode t)
+
+(add-to-list 'load-path "/home/troden/code/emacs-async")
+(add-to-list 'load-path "/home/troden/code/helm")
+
+; (require 'helm-config)
+
+
+(use-package helm-config
+    :config
+  (progn
+    (helm-mode 1)
+    (helm-adaptive-mode 1)
+    (helm-push-mark-mode 1)
+    (load "/home/troden/code/dot-files/helm-init.el")))
+
+;; 					; (global-set-key (kbd "M-x") 'helm-M-x)
+;; (helm-mode 1)
+
+
+(defvar quicklisp-path "/home/troden/quicklisp")
+(load (concat quicklisp-path "/slime-helper"))
+
+(slime-setup '(slime-fancy slime-mrepl slime-banner slime-tramp
+	       slime-xref-browser slime-highlight-edits
+	       slime-sprof))
+;;
+;; Decide where to put a slime scratch file, if you want one, and set
+;; it up with:
+;;
+(setf slime-scratch-file "/home/troden/.slime-scratch.lisp")
+;;
+
+(defadvice slime-scratch
+    (after slime-scratch-adjust-modes () activate compile)
+  (turn-some-mode-off)
+  (turn-some-other-mode-on))
+
+(use-package hyde
+  :config
+  (progn
+    (setq hyde-home "/home/troden/code/blog")
+    (setq hyde_images_dir "static")
+    ))
+  
+
+
 
