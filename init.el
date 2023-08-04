421d7d981744ee00dd8f4a2cbc936bc2ef77368f
Ted Roden <tedroden@gmail.com>
2023-08-03 23:28:44 -0400

    read only tmux, removing stuff from emacs

--- init.el
+++ init.el
@@ -294,9 +294,9 @@
 (use-package counsel
   :bind
   (
-  ("C-x b" . 'counsel-switch-buffer)
-  ("M-x" . 'counsel-M-x)
-  ("C-x C-f" . 'counsel-find-file)
+   ("C-x b" . 'counsel-switch-buffer)
+   ("M-x" . 'counsel-M-x)
+   ("C-x C-f" . 'counsel-find-file)
    ("C-x d" . 'counsel-dired)
    ("C-h f" . 'counsel-describe-function)
    ("C-h v" . 'counsel-describe-variable)
@@ -385,6 +385,10 @@
   :bind (("C-\-" . 'mc/mark-next-like-this)
 		 ("C-0" . 'mc/unmark-next-like-this)))
 
+(defvar org-default-tasks-file nil "Default org tasks file.")
+(defvar fh-file nil "Fancy Hands task file.")
+(defvar grow-file nil "Fancy Hands task file.")
+
 (use-package org
   :ensure t
   :demand t
@@ -400,6 +404,10 @@
           ("<tab>" . org-cycle)
           ("S-<tab>" . org-shifttab)
           ("C-<tab>" . org-global-cycle)
+
+		  ("M-P" . org-metaup)
+		  ("M-N" . org-metadown)
+
 		  ("C-c t i" . org-table-insert-row)
 		  ("C-c t p" . org-table-move-row-up)
 		  ("C-c t n" . org-table-move-row-down)
@@ -411,14 +419,18 @@
   (setq org-directory (file-truename "~/Dropbox/Org"))
   (setq org-archive-location "archive/%s_archive::")
   (setq org-agenda-files (list org-directory))
-  (setq org-default-notes-file (concat org-directory "/Notes.org"))
   (setq org-default-tasks-file (concat org-directory "/Tasks.org"))
+  (setq fh-file (concat org-directory "/Fancy Hands.org"))
+  (setq grow-file (concat org-directory "/Grow.org"))
   :config
-  (setq org-capture-templates
-        '(("t" "Todo" entry (file+headline org-default-tasks-file "Tasks")
-           "* TODO %?\n  %i\n  %a")
-          ("n" "Note" entry (file+headline org-default-notes-file "Notes")
-           "* %?\n  %i\n  %a")))
+(setq org-capture-templates
+      '(("t" "TODO" entry (file org-default-tasks-file)
+         "* TODO %?\n  %i\n  %a")
+        ("f" "Fancy Hands" entry (file fh-file)
+         "* TODO %?\n  %i\n  %a")
+        ("g" "Grow" entry (file grow-file)
+         "* TODO %?\n  %i\n  %a")
+        ))
   (require 'org-agenda))
 
 
@@ -522,216 +534,22 @@
   :config
   (pdf-tools-install))
 
-;; ;;;;;;;;;;;;;;;;;;;;
-;; ;; Enable vertico (config from vertico/README.org)
-;; ;; START OF VERTICO CONFIG
-;; (use-package vertico
-;;   :init
-;;   (vertico-mode)
 
-;;   ;; Different scroll margin
-;;   ;; (setq vertico-scroll-margin 0)
 
-;;   ;; Show more candidates
-;;   ;; (setq vertico-count 20)
-
-;;   ;; Grow and shrink the Vertico minibuffer
-;;   ;; (setq vertico-resize t)
-
-;;   ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
-;;   ;; (setq vertico-cycle t)
-;;   )
-
-;; ;; Persist history over Emacs restarts. Vertico sorts by history position.
-;; (use-package savehist
-;;   :init
-;;   (savehist-mode))
-
-;; ;; A few more useful configurations...
-;; (use-package emacs
-;;   :init
-;;   ;; Add prompt indicator to `completing-read-multiple'.
-;;   ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-;;   (defun crm-indicator (args)
-;;     (cons (format "[CRM%s] %s"
-;;                   (replace-regexp-in-string
-;;                    "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-;;                    crm-separator)
-;;                   (car args))
-;;           (cdr args)))
-;;   (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
-
-;;   ;; Do not allow the cursor in the minibuffer prompt
-;;   (setq minibuffer-prompt-properties
-;;         '(read-only t cursor-intangible t face minibuffer-prompt))
-;;   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-;;   ;; Enable recursive minibuffers
-;;   (setq enable-recursive-minibuffers t))
-
-;; (use-package orderless
-;;   :ensure t
-;;   :custom
-;;   (completion-styles '(orderless basic))
-;;   (completion-category-overrides '((file (styles basic partial-completion)))))
-
-;; ;; END OF VERTICO CONFIG
-;; ;;;;;;;;;;;;;;;;;;;;
-
-;; ;; Enable rich annotations using the Marginalia package
-;; (use-package marginalia
-;;   ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
-;;   ;; available in the *Completions* buffer, add it to the
-;;   ;; `completion-list-mode-map'.
-;;   :bind (:map minibuffer-local-map
-;; 			  ("M-A" . marginalia-cycle))
-
-;;   ;; The :init section is always executed.
-;;   :init
-
-;;   ;; Marginalia must be actived in the :init section of use-package such that
-;;   ;; the mode gets enabled right away. Note that this forces loading the
-;;   ;; package.
-;;   (marginalia-mode))
-
-;; (use-package nerd-icons-completion
-;;   :after marginalia
-;;   :hook (marginalia-mode . nerd-icons-completion-marginalia-setup)
-;;   :config
-;;   (nerd-icons-completion-mode))
-
-;; (use-package consult
-
-;; ;; Example configuration for Consult
-;; (use-package consult
-;;   ;; Replace bindings. Lazily loaded due by `use-package'.
-;;   :bind (;; C-c bindings in `mode-specific-map'
-;;          ("C-c M-x" . consult-mode-command)
-;;          ("C-c h" . consult-history)
-;;          ("C-c k" . consult-kmacro)
-;;          ("C-c i" . consult-info)
-;;          ([remap Info-search] . consult-info)
-;;          ;; C-x bindings in `ctl-x-map'
-;;          ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
-;;          ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
-;;          ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-;;          ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-;;          ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
-;; 		 ;;         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
-;;          ;; Custom M-# bindings for fast register access
-;;          ("M-#" . consult-register-load)
-;;          ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
-;;          ("C-M-#" . consult-register)
-;;          ;; Other custom bindings
-;;          ("M-y" . consult-yank-pop)                ;; orig. yank-pop
-;;          ;; M-g bindings in `goto-map'
-;; 		 ;;         ("M-g e" . consult-compile-error)
-;; 		 ;;         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
-;;          ("M-g" . consult-goto-line)             ;; orig. goto-line
-;;          ;; ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
-;;          ;; ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
-;;          ;; ("M-g m" . consult-mark)
-;;          ;; ("M-g k" . consult-global-mark)
-;;          ;; ("M-g i" . consult-imenu)
-;;          ;; ("M-g I" . consult-imenu-multi)
-;;          ;; M-s bindings in `search-map'
-;;          ("M-s d" . consult-find)
-;;          ("M-s D" . consult-locate)
-;;          ("M-s g" . consult-grep)
-;;          ("M-s G" . consult-git-grep)
-;;          ("M-s r" . consult-ripgrep)
-;;          ("M-s l" . consult-line)
-;;          ("M-s L" . consult-line-multi)
-;;          ("M-s k" . consult-keep-lines)
-;;          ("M-s u" . consult-focus-lines)
-;;          ;; Isearch integration
-;;          ("M-s e" . consult-isearch-history)
-;;          :map isearch-mode-map
-;;          ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
-;;          ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
-;;          ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
-;;          ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
-;;          ;; Minibuffer history
-;;          :map minibuffer-local-map
-;;          ("M-s" . consult-history)                 ;; orig. next-matching-history-element
-;;          ("M-r" . consult-history))                ;; orig. previous-matching-history-element
-
-;;   ;; Enable automatic preview at point in the *Completions* buffer. This is
-;;   ;; relevant when you use the default completion UI.
-;;   :hook (completion-list-mode . consult-preview-at-point-mode)
-
-;;   ;; The :init configuration is always executed (Not lazy)
-;;   :init
-
-;;   ;; Optionally configure the register formatting. This improves the register
-;;   ;; preview for `consult-register', `consult-register-load',
-;;   ;; `consult-register-store' and the Emacs built-ins.
-;;   (setq register-preview-delay 0.5
-;;         register-preview-function #'consult-register-format)
-
-;;   ;; Optionally tweak the register preview window.
-;;   ;; This adds thin lines, sorting and hides the mode line of the window.
-;;   (advice-add #'register-preview :override #'consult-register-window)
-
-;;   ;; Use Consult to select xref locations with preview
-;;   (setq xref-show-xrefs-function #'consult-xref
-;;         xref-show-definitions-function #'consult-xref)
-
-;;   ;; Configure other variables and modes in the :config section,
-;;   ;; after lazily loading the package.
-;;   :config
-
-;;   ;; Optionally configure preview. The default value
-;;   ;; is 'any, such that any key triggers the preview.
-;;   ;; (setq consult-preview-key 'any)
-;;   ;; (setq consult-preview-key "M-.")
-;;   ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
-;;   ;; For some commands and buffer sources it is useful to configure the
-;;   ;; :preview-key on a per-command basis using the `consult-customize' macro.
-;;   (consult-customize
-;;    consult-theme :preview-key '(:debounce 0.2 any)
-;;    consult-ripgrep consult-git-grep consult-grep
-;;    consult-bookmark consult-recent-file consult-xref
-;;    consult--source-bookmark consult--source-file-register
-;;    consult--source-recent-file consult--source-project-recent-file
-;;    ;; :preview-key "M-."
-;;    :preview-key '(:debounce 0.4 any))
-
-;;   ;; Optionally configure the narrowing key.
-;;   ;; Both < and C-+ work reasonably well.
-;;   ;; doesn't work (?)
-;;   (setq consult-narrow-key "<") ;; "C-+"
-
-;;   ;; Optionally make narrowing help available in the minibuffer.
-;;   ;; You may want to use `embark-prefix-help-command' or which-key instead.
-;;   ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
-
-;;   ;; By default `consult-project-function' uses `project-root' from project.el.
-;;   ;; Optionally configure a different project root function.
-;;   ;;;; 1. project.el (the default)
-;;   ;; (setq consult-project-function #'consult--default-project--function)
-;;   ;;;; 2. vc.el (vc-root-dir)
-;;   ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
-;;   ;;;; 3. locate-dominating-file
-;;   ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
-;;   ;;;; 4. projectile.el (projectile-project-root)
-;;   ;; (autoload 'projectile-project-root "projectile")
-;;   ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
-;;   ;;;; 5. No project support
-;;   ;; (setq consult-project-function nil)
-;;   )
-
+(defun ok-goto-line (line)
+  (goto-char (point-min))
+  (forward-line (1- line)))
 
 ;; This functoin is mostly from: https://gist.github.com/magnars/3292872
 (defun goto-line-with-feedback (&optional line)
-  "Show line numbers temporarily, while prompting for the line number input"
+  "Show line numbers temporarily, while prompting for the line number input. LINE: number"
   (interactive "P")
-  (if line
-      (goto-line line)
+(if line
+	(ok-goto-line line)
     (unwind-protect
         (progn
           (nlinum-mode 1)
-          (goto-line (read-number "Goto line: ")))
+          (ok-goto-line (read-number "Goto line: ")))
       (nlinum-mode -1))))
 
 (use-package nlinum
@@ -765,7 +583,15 @@
 (setq tramp-auto-save-directory (expand-file-name "~/.emacs.d/tramp-autosave"))
 
 (server-start)
+;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
+(custom-set-variables
+  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
 
 ;; finally
 (org-roam-dailies-goto-today)
-;; ;;; init.el ends here
+
+;;; init.el ends here
