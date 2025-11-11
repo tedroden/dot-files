dfb0d606e08c7be7901d7bd43b16e1ff28e04df9
Ted Roden <tedroden@gmail.com>
2025-11-10 20:42:59 -0500

    [tmux] `prefix S` saves the transcript to ~/Dropbox/mem/tmux/...

---
+++ tmux/tmux-save-pane.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+SAVEDIR=~/Dropbox/mem/tmux/$(date +%Y/%m/%d)
+mkdir -p "$SAVEDIR"
+
+SESSION=$(tmux display-message -p '#{session_name}')
+WINDOW=$(tmux display-message -p '#{window_index}')
+PANE=$(tmux display-message -p '#{pane_index}')
+TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+
+FILENAME="tmux-${TIMESTAMP}-${SESSION}-w${WINDOW}-p${PANE}.txt"
+
+tmux capture-pane -S -32768
+tmux save-buffer "${SAVEDIR}/${FILENAME}"
+
+echo "Saved: ${SAVEDIR}/${FILENAME}"
